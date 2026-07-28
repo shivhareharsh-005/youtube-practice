58c497c8717460caf7222702733ea597f27a6e61
Harsh <shivhareharsh027@gmail.com>
2026-07-28 18:58:23 +0530

    feat: Implement user authentication with JWT and password hashing

--- Backend/src/modles/user.models.js
+++ Backend/src/modles/user.models.js
@@ -1,6 +1,8 @@
 
+import 'dotenv/config'
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const userSchema = new mongoose.Schema({
     fullName : {
         type : String,
@@ -38,14 +40,57 @@ const userSchema = new mongoose.Schema({
     timestamps : true
 })
 
+//  password incription middleware....
 userSchema.pre("save", async function(){
     if(!this.isModified("password")) return ;
 
-    this.password = await bcrypt.hash(this.password, 10);
-    
-    
+    this.password = await bcrypt.hash(this.password, 10); 
 })
 
+//   password matvhing method
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return bcrypt.compare(
+      password,
+      this.password
+    )
+}
+
+// generate access token ....
+userSchema.methods.generateAccessToken = async function () {
+    jwt.sign(
+        // payload
+        {
+           _id : this._id,
+           email : this.email,
+           username : this.username,
+           fullName : this.fullName
+        },
+        // secret key
+        process.env.ACCESS_TOKEN_SECRET,
+        // token expiry
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+    jwt.sign(
+        // payload
+        {
+            _id = this.id
+        },
+        // secret key
+
+        process.env.REFRESH_TOKEN_SECRET,
+
+        // token expiry
+
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+} 
 
 
 const User = model.Schema("User", userSchema);
